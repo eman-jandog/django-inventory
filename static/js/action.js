6d54637b5aaf10c51eaa8cd6f93c3c88eca51be2
Name-San <eman.jandog@gmail.com>
2025-09-28 20:27:50 +0800

    working with side bar

--- static/js/action.js
+++ static/js/action.js
@@ -1,29 +1,29 @@
 function toggleSidebarMode() {
     const sidebar = document.getElementById('sidebar');
     const compactSidebar = document.getElementById('compactSidebar');
-    const fullSidebarContent = document.getElementById('fullSidebarContent');
+    const items = compactSidebar.querySelectorAll('.btn-title')
     // const mainContent = document.getElementById('mainContent');
-    
-    const isCompact = !fullSidebarContent.classList.contains('hidden');
-    
+
+    const isCompact = !sidebar.classList.contains('w-64');
+    console.log(isCompact)
+
     if (isCompact) {
         // Switch to compact sidebar
-        compactSidebar.classList.remove('hidden');
-        compactSidebar.classList.add('lg:flex');
-        fullSidebarContent.classList.add('hidden');
-        sidebar.classList.remove('w-64');
-        sidebar.classList.add('w-16');
-        // mainContent.classList.remove('lg:ml-64');
-        // mainContent.classList.add('lg:ml-16');
-    } else {
-        // Switch to full sidebar
-        compactSidebar.classList.remove('lg:flex');
-        compactSidebar.classList.add('hidden');
-        fullSidebarContent.classList.remove('hidden');
+        items.forEach(item => {
+            item.className = 'btn-title absolute font-medium ml-8'
+        })
         sidebar.classList.remove('w-16');
         sidebar.classList.add('w-64');
-        // mainContent.classList.remove('lg:ml-16');
-        // mainContent.classList.add('lg:ml-64');
+        
+
+    } else {
+        // Switch to full sidebar
+        items.forEach(item => {
+            item.className = 'btn-title absolute left-full ml-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap'  
+        })
+        sidebar.classList.remove('w-64');
+        sidebar.classList.add('w-16');
+        
     }
 }
 
